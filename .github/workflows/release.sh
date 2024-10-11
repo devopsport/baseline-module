10b886b1c120d0480d0a923862639475e59e0be4
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2024-10-10 23:31:56 -0500

    cambiando logica de release

---
+++ .github/workflows/release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo ${GIT_TOKEN} | gh auth login --with-token
+gh repo clone punkerside/${GITHUB_REPO}
+cd ${GITHUB_REPO}/
+GITHUB_TAGS=$(gh release list)
+
+if [ "$(gh release list | grep Latest | awk '{print $9 " " $2}')" == " Latest" ]
+then
+  MAJOR_VERSION=$(gh release list | grep Latest | awk '{print $9 " " $1}' | cut -d "v" -f2 | cut -d "." -f1)
+  MINOR_VERSION=$(gh release list | grep Latest | awk '{print $9 " " $1}' | cut -d "v" -f2 | cut -d "." -f2)
+  PATCH_VERSION=$(gh release list | grep Latest | awk '{print $9 " " $1}' | cut -d "v" -f2 | cut -d "." -f3)
+  PATCH_VERSION=$((PATCH_VERSION+1))
+  gh release create v${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION} --generate-notes
+else
+  gh release create v0.0.1 --generate-notes
+fi
