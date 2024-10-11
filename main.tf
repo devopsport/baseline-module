67d884f5fa423bcc04fcb8bf1c4cb5dc566a1f54
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2024-10-11 18:03:42 -0500

    depurando

--- main.tf
+++ main.tf
@@ -3,38 +3,3 @@ resource "null_resource" "main" {
     command = "echo holaMundo"
   }
 }
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_security_group" "main" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "main" {
-  security_group_id = aws_security_group.main.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
-}
-
-resource "aws_vpc_security_group_egress_rule" "main" {
-  security_group_id = aws_security_group.main.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
-}
-
-resource "aws_ecr_repository" "main" {
-  name                 = "bar"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = false
-  }
-}
